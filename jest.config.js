c9140c63c2659d14e4960fe467008c896267d549
under_moon <under-moon@outlook.com>
2021-04-08 20:45:02 +0800

    fix: ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  globals: {
-    // work around: https://github.com/kulshekhar/ts-jest/issues/748#issuecomment-423528659
-    'ts-jest': {
-      diagnostics: {
-        ignoreCodes: [151001]
-      }
-    }
-  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
